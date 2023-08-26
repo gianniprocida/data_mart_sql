b0fc012444e8c3b080913974bee214ff70243127
Gianni Procida <gianniprocida@Giannis-MacBook-Pro.local>
2023-08-26 17:49:26 +0200

    cleaning-up

--- api/app.py
+++ api/app.py
@@ -3,6 +3,8 @@ from flask import jsonify
 from flask import Flask, render_template, request
 import os 
 from menu import Filter, Filter_by_price, Filter_by_location, Filter_by_rating
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -64,6 +66,68 @@ def filter_by_price_route():
         
         return jsonify(dict_data) 
     return render_template('filter_price.html')
+
+
+@app.route('/update/users',methods=['GET','POST'])
+def update_users():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        phone = request.form['phone_number']
+        dob_str = request.form['DoB']
+        gender = request.form['gender']
+        country = request.form['country']
+        user_type = request.form['user_type']
+        
+
+        #Validate DoB as a date format
+        try:
+            dob = datetime.datetime.strptime(dob_str, '%Y-%m-%d')
+        except ValueError:
+            return 'Invalid date format for DoB',400
+        
+        gender = gender.lower()
+        user_type = user_type.lower()
+        
+        #Validate gender
+        if gender not in ["male","female"]:
+            return 'Invalid gender value',400
+        
+        # Validate user_typr
+        if user_type not in ["guest","host"]:
+            return 'Invalid user_type'
+
+        
+        obj = Filter()
+        cur = obj.cur
+        cnx = obj.cnx
+
+        print("Ready to load data to mysql database...")
+        
+        #Query the max id in the Users table
+        cur.execute("""select max(id) from Users""")
+        res = cur.fetchone()
+        max_id = res[0] + 1
+
+       
+         
+        cur.execute("""INSERT INTO Users (id, first_name, last_name, email, phone_number, DoB, gender, country, user_type)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", (max_id, first_name, last_name, email, phone, dob, gender, country, user_type))
+        
+        cnx.commit()
+        
+
+        query = "SELECT * FROM Users ORDER BY id DESC LIMIT 1"
+
+        cur.execute(query)
+
+        res = cur.fetchone()
+
+        print(res)
+
+
+
 
 
 
